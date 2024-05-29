b1867439853e489d1fbadbdb6f8acbd355217e28
francomarinozzi <francomarinozzi4@gmail.com>
2024-05-28 21:30:54 -0300

    contratos agregados

--- password_gen.py
+++ password_gen.py
@@ -3,6 +3,14 @@ import string
 
 
 def create_psw(psw_len):
+    """  generates a password composed of random numbers and letters of length *psw_len*
+
+    Args:
+        psw_len (int): length of the password.
+
+    Returns:
+        str: generated password
+    """
     password = ""
     if psw_len == 1:
         password += str(random.randint(0,9))
@@ -14,8 +22,11 @@ def create_psw(psw_len):
 
 
 def create_psw_txt():
+    """generate a .txt in the src folder an write a password into it
+    """
     doc = open("password.txt", "w")
     doc.write(create_psw(10))
     doc.close()
+    
 
 create_psw_txt()
